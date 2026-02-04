800d64a080143c92a868ee4f53bf7da47ebe8f77
Sebastien Gault <sgault.webdev@gmail.com>
2026-02-04 01:15:55 +0100

    fix: set cors headers to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,6 +13,20 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+if (process.env.NODE_ENV === 'development') {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+
+    next();
+  });
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
